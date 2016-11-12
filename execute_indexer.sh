d92e095210cc8c58ae52fb9065c9794d84776eb1
sorrowbeaver <jkj8790@gmail.com>
2016-11-12 10:54:31 +0900

    실행 스크립트 수정

--- execute_indexer.sh
+++ execute_indexer.sh
@@ -4,25 +4,7 @@ STEP_OPTION=$2
 rm ./a.out > /dev/null 2>&1
 rm ~/Dropbox/ir/result.txt > /dev/null 2>&1
 
-if [ "$OPTION" == "-r" ]
-then
-	g++ -std=c++11 -O3 *.cpp
-	if [ "$STEP_OPTION" == "-s1" ]
-	then
-		if ./a.out input/ index/ result/ -s1; then
-			echo "Start execution"
-		fi
-	elif [ "$STEP_OPTION" == "-s2" ]
-	then
-		if ./a.out input/ index/ result/ -s2; then
-			echo "Start execution"
-		fi
-	else
-		if ./a.out input/ index/ result/; then
-			echo "Start execution"
-		fi
-	fi
-elif [ "$OPTION" == "-d" ]
+if [ "$OPTION" == "-d" ]
 then
 	if g++ -std=c++11 -O3 *.cpp && ./a.out -d; then
 		echo "Start execution"
